199d4d2af0cf8cda01ba69996b01f7e27f66cd09
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-02-23 19:49:12 +0200

    Added header/initialized state management

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# decency_starting-point
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ components/layout/header.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
+
+export const Header = () => {
+  return (
+    <ContentWrapper>
+      <HeaderWrapper>
+        <ul>
+          <Link passHref={true} href="/">
+            <li>Setup</li>
+          </Link>
+          <a href="#how-to">
+            <li>How to</li>
+          </a>
+        </ul>
+        <Image src="/logo.svg" alt="logo" width={85} height={85} />
+        <ul>
+          <a>
+            <li>Login</li>
+          </a>
+        </ul>
+      </HeaderWrapper>
+    </ContentWrapper>
+  );
+};
+
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 70rem;
+  width: 100%;
+  justify-content: space-between;
+
+  ul {
+    text-decoration: none;
+    list-style: none;
+    display: flex;
+    gap: 1.1rem;
+    padding: 0;
+
+    li {
+      cursor: pointer;
+
+      &:hover {
+        color: grey;
+        text-decoration: underline;
+      }
+    }
+  }
+`;
